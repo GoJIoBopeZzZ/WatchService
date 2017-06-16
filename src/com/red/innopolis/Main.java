43b87350fad8fdfaeecbdcedf6e94b02298968b2
_ReD_ <apple.tyomka@gmail.com>
2017-06-16 10:30:41 +0300

    Complete WatchService

---
+++ src/com/red/innopolis/Main.java
@@ -0,0 +1,34 @@
+package com.red.innopolis;
+
+import java.io.BufferedWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Path myPath =
+                Paths.get("/Users/_red_/IdeaProjects/NewInputOutput/src/com/red/innopolis/test.txt");
+    
+        try {
+            List<String> content = Files.readAllLines(myPath);
+            System.out.println(content);
+        } catch (Exception e) {}
+    
+        try {
+            BufferedWriter bw = Files.newBufferedWriter(myPath,
+                    StandardCharsets.UTF_8,
+                    StandardOpenOption.WRITE
+            );
+            bw.write("Bye");
+            bw.flush();
+            bw.close();
+//            Path myPath2 = Files.createFile(secondFile, myFileAttribute);
+            List<String> content = Files.readAllLines(myPath);
+            System.out.println(content);
+        } catch (Exception e) {}
+    
+    }
+}
---
+++ src/com/red/innopolis/WatcherService.java
@@ -0,0 +1,37 @@
+package com.red.innopolis;
+
+import java.nio.file.*;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+import java.io.*;
+import java.nio.file.WatchService;
+import java.util.*;
+
+/**
+ * Created by _red_ on 16.06.17.
+ */
+public class WatcherService {
+    public static void main (String[] args) {
+        for (; ; )
+            try {
+                WatchService watcher = FileSystems.getDefault().newWatchService();
+        
+                Path directory = Paths.get("/Users/_red_/IdeaProjects/NewInputOutput/src/com/red/innopolis");
+                try {
+                    WatchKey key = directory.register(watcher, ENTRY_MODIFY);
+                    while (true) {
+                        key = watcher.take();
+                        for (WatchEvent<?> event : key.pollEvents()) {
+                            if (event.kind() == ENTRY_MODIFY) System.out.println("Files Modify!!!");
+                        }
+                        key.reset();
+                    }
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+                
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+    }
+}
